72abd326107cdbd4ab7c4e9050ee0fe5cb467b60
inert-one <shubhamdubey57@gmail.com>
2021-05-14 11:49:26 +0530

    build

---
+++ build/precache-manifest.7022bfb1c74ad95bd6abb7335b9018cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4da1ea95ade2e5b435142cbce2b95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec80b1639c0752e885b2",
+    "url": "/static/css/main.2d4ac06c.chunk.css"
+  },
+  {
+    "revision": "1adc3126e6dd49eef5b2",
+    "url": "/static/js/2.f5b65d7c.chunk.js"
+  },
+  {
+    "revision": "ec80b1639c0752e885b2",
+    "url": "/static/js/main.45818e50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e655bee74d9ec4da397956acf8779135",
+    "url": "/static/media/cancel.e655bee7.svg"
+  }
+]);
